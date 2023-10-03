b6273bd53f1219e76b65e92cdbeca87ec5712352
Windokk <68743762+Windokk@users.noreply.github.com>
2023-10-03 20:11:47 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from tkinter import ttk
+import ctypes as ct
+
+def dark_title_bar(window):
+    """
+    Sets windows title bar to dark color
+    For more infos:
+    https://learn.microsoft.com/en-us/windows/win32/api/dwmapi/ne-dwmapi-dwmwindowattribute
+    """
+    window.update()
+    set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
+    get_parent = ct.windll.user32.GetParent
+    hwnd = get_parent(window.winfo_id())
+    value = 2
+    value = ct.c_int(value)
+    set_window_attribute(hwnd, 20, ct.byref(value),4)
+
+root = Tk()
+root.title("Kold Launcher")
+root.geometry("900x700")
+root.configure(background="#292929")
+dark_title_bar(root)
+
+tabControl = ttk.Notebook(root)
+  
+tab1 = ttk.Frame(tabControl)
+tab2 = ttk.Frame(tabControl)
+  
+tabControl.add(tab1, text ='Tab 1')
+tabControl.add(tab2, text ='Tab 2')
+tabControl.pack(expand = 1, fill ="both")
+  
+ttk.Label(tab1, 
+          text ="Welcome to \
+          GeeksForGeeks").grid(column = 0, 
+                               row = 0,
+                               padx = 30,
+                               pady = 30)  
+ttk.Label(tab2,
+          text ="Lets dive into the\
+          world of computers").grid(column = 0,
+                                    row = 0, 
+                                    padx = 30,
+                                    pady = 30)
+
+
+
+root.mainloop()
