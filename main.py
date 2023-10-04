a3764063e784c69e48e7e96bcd40474cbe72d268
Windokk <68743762+Windokk@users.noreply.github.com>
2023-10-04 14:01:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from tkinter import *
-from tkinter import ttk
+import ttkbootstrap as ttk
 import ctypes as ct
 
 def dark_title_bar(window):
@@ -16,34 +15,25 @@ def dark_title_bar(window):
     value = ct.c_int(value)
     set_window_attribute(hwnd, 20, ct.byref(value),4)
 
-root = Tk()
+dark_grey = "#292929"
+
+root = ttk.Window(themename="darkly")
 root.title("Kold Launcher")
 root.geometry("900x700")
-root.configure(background="#292929")
+root.resizable(False, False)
 dark_title_bar(root)
 
-tabControl = ttk.Notebook(root)
-  
-tab1 = ttk.Frame(tabControl)
-tab2 = ttk.Frame(tabControl)
-  
-tabControl.add(tab1, text ='Tab 1')
-tabControl.add(tab2, text ='Tab 2')
-tabControl.pack(expand = 1, fill ="both")
-  
-ttk.Label(tab1, 
-          text ="Welcome to \
-          GeeksForGeeks").grid(column = 0, 
-                               row = 0,
-                               padx = 30,
-                               pady = 30)  
-ttk.Label(tab2,
-          text ="Lets dive into the\
-          world of computers").grid(column = 0,
-                                    row = 0, 
-                                    padx = 30,
-                                    pady = 30)
+style = ttk.Style()
+style.configure('lefttab.TNotebook', tabposition='wn')
+tabController = ttk.Notebook(root, style='lefttab.TNotebook')
 
+KV = ttk.Frame(tabController, width=500, height=300)
+KV_Online = ttk.Frame(tabController, width=500, height=300)
+KV_Competitive = ttk.Frame(tabController, width=500, height=300)
 
+tabController.add(KV, text='Play Kold Vendetta')
+tabController.add(KV_Online, text='Play Kold Vendetta : Online Mode')
+tabController.add(KV_Competitive, text='Play Kold Vendetta : Competitive')
+tabController.pack(expand=1, fill="both")
 
 root.mainloop()
